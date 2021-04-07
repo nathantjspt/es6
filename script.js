1deb34a5c346bcdec1da880435a091794bf12f0c
Nathan Tjokrosaputra <nthnn060500@gmail.com>
2021-04-07 13:21:08 +0700

    Learning map and filter

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+// Ambil element
+const skill = document.getElementById("skills");
+const primarySkill = document.getElementById("primarySkills");
+
+const mySkills = ["ES6", "Git", "Web Development", "Web Design"];
+
+// Map
+// Undefined
+const printSkillsForEach = mySkills.forEach((skill)=> {
+    return skill;
+});
+
+// Benar
+const printSkillsMap = mySkills.map((skill)=> {
+    return skill;
+});
+
+skills.innerHTML = printSkillsMap;
+
+// Filter
+const printPrimarySkill = mySkills.filter((skill)=>{
+    return skill == "ES6";
+});
+
+primarySkill.innerHTML = printPrimarySkill;
