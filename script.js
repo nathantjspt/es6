eaf2ab9ef86a76b264548650dcafeb7a8055ed78
Nathan Tjokrosaputra <nthnn060500@gmail.com>
2021-04-07 10:56:18 +0700

    Inisialisasi 2-let

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('hello world')
+// ES5
+// var myName = "Ariq";
+// myName = "Richard";
+// console.log(myName);
+
+var myCarsVar = ["BMW", "Tesla", "Toyota"]
+
+for (var i = 0; i < myCarsVar.length; i++){
+    console.log("My Cars var = " + myCarsVar[i]);
+}
+
+console.log(i);
+
+// ES6
+// let myNameLet = "Ariq"
+// myNameLet = "Calvin"
+// console.log(myNameLet)
+
+const myCarsLet = ["BMW", "Tesla", "Toyota"];
+
+for (let x = 0; x < myCarsVar.length; x++){
+    console.log("My Cars let = " + myCarsVar[x]);
+}
+
+console.log(x);
+
+// let myNameFunc = "hello"
+
+// function abc(){
+//     console.log(myNameFunc);
+// }
+
+// abc();
+// console.log(myNameFunc)
+
