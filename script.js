5788570b17f705a6ab3376da6e5950c221a4310b
Nathan Tjokrosaputra <nthnn060500@gmail.com>
2021-04-07 11:20:26 +0700

    belajar object literal

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('hello world')
+// OBJECT LITERALS
+const cart = document.getElementById("cart")
+
+// ES5
+// function addProduct(name, category){
+//     return{
+//         name: name,
+//         category: category
+//     }
+// }
+
+// var getProduct = addProduct("iPhone 12", "Smartphone");
+
+// console.log(getProduct);
+
+// cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}`;
+
+// ES6
+function addProduct(name, category){
+    return{name, category};
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone");
+
+console.log(getProduct);
+
+cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}`;
