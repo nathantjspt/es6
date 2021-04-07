58f151e9bc34140d1ff7a39eab7fc153c421fa0a
Nathan Tjokrosaputra <nthnn060500@gmail.com>
2021-04-07 11:48:06 +0700

    belajar foreach

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('hello world')
+// Ambil element
+const skills = document.getElementById("skills");
+
+// Buat variabel
+const mySkills = ["ES6", "UX Design", "Git"]
+
+// mySkills.pop("Git");
+
+function showSkill(mySkill){ // Normal function
+    console.log(mySkill);
+}
+
+const showSkill2 = (mySkill) => { // Arrow function
+    console.log(mySkill);
+};
+
+//forEach
+mySkills.forEach(function (mySkill){
+    console.log(mySkill);
+});
+
+let parent = "<ul>";
+
+mySkills.forEach((mySkill)=>{
+    // console.log(mySkill);
+    parent += `<li>${mySkill}</li>`;
+});
+
+parent += "</ul>";
+
+mySkills.forEach(showSkill2);
+skills.innerHTML = parent;
