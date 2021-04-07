073c08196ff2593c9c940fde91775e7ef8d71431
Nathan Tjokrosaputra <nthnn060500@gmail.com>
2021-04-07 13:45:19 +0700

    Learn how to fetch data from api

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+// Ambil element
+const products = document.getElementById("products")
+const allProducts = "https://fakestoreapi.com/products";
+
+function createNode(element){
+    return document.createElement(element)
+}
+
+function append(parent, el){
+    return parent.appendChild(el);
+}
+
+fetch(allProducts)
+.then(Response => Response.json())
+.then((json)=> {
+    json.map((product)=>{
+        let li = createNode("li")
+        let img = createNode("img")
+        let span = createNode("span")
+
+        img.setAttribute("width", "200")
+
+        img.src = product.image;
+        span.innerHTML = `${product.title}, ${product.price}`
+
+        append(li, img);
+        append(li, span);
+        append(products, li);
+    })
+})
+.catch((error)=>console.log(error));
